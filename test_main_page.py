a16cccaf13d80df936cbf0b58e16490033733108
ZHMESHNECK <johndoe@example.com>
2023-03-07 13:22:17 +0200

    marl - login_test

--- test_main_page.py
+++ test_main_page.py
@@ -1,34 +1,27 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
-import time
+from pages.product_page import ProductPage
 
-'''def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-    page = LoginPage(browser, link)
-    page.open()                      
-    # page.go_to_login_page()
-    page.should_be_login_page()
-'''
+import pytest
 
 
-## ! 1 способ перехода на другую страницу
-'''def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()'''
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-## ! 2
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com'
